a2832e1bca02af2021419e400c962ed841321d07
Wasim <wasimakthar.work@gmail.com>
2023-04-13 16:14:17 +0530

    Seventh Commit: Simplified products and news

--- fruitkha/src/app/components/news/news.component.ts
+++ fruitkha/src/app/components/news/news.component.ts
@@ -1,10 +1,39 @@
 import { Component } from '@angular/core';
-
+interface newsList{
+newsImg: string,
+newsTopic: string,
+newsAuthor: string,
+newsDate: string,
+newsContent: string,
+}
 @Component({
   selector: 'app-news',
   templateUrl: './news.component.html',
   styleUrls: ['./news.component.css']
 })
 export class NewsComponent {
+  newsDetails: newsList[]=[
+    {
+    newsImg: '/assets/images/news1.jpg',
+    newsTopic: 'You will vainly look for fruit on it in autumn.',
+    newsAuthor: 'Admin',
+    newsDate: '10/02/2022',
+    newsContent: 'Vivamus lacus enim, pulvinar vel nulla sed, scelerisque rhoncus nisi. Praesent vitae mattis nunc, egestas viverra eros.',
+    },
+    {
+      newsImg: '/assets/images/news2.jpg',
+      newsTopic: "A man's worth has its season, like tomato.",
+      newsAuthor: 'Admin',
+      newsDate: '10/02/2022',
+      newsContent: 'Vivamus lacus enim, pulvinar vel nulla sed, scelerisque rhoncus nisi. Praesent vitae mattis nunc, egestas viverra eros.',
+      },
+      {
+        newsImg: '/assets/images/news3.jpg',
+        newsTopic: 'Good thoughts bear good fresh juicy fruit.',
+        newsAuthor: 'Admin',
+        newsDate: '10/02/2022',
+        newsContent: 'Vivamus lacus enim, pulvinar vel nulla sed, scelerisque rhoncus nisi. Praesent vitae mattis nunc, egestas viverra eros.',
+        }
+  ]
 
 }
--- fruitkha/src/app/components/tabnews/tabnews.component.ts
+++ fruitkha/src/app/components/tabnews/tabnews.component.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
-
+interface newsList{
+  newsImg: string,
+  newsTopic: string,
+  newsAuthor: string,
+  newsDate: string,
+  newsContent: string,
+  }
 @Component({
   selector: 'app-tabnews',
   templateUrl: './tabnews.component.html',
@@ -12,5 +18,49 @@ export class TabnewsComponent {
     {img:"/assets/images/logo3.png",src:"logos"} ,
     {img:"/assets/images/logo4.png",src:"logos"} , 
     {img:"/assets/images/logo5.png",src:"logos"}  
+  ]
+  newsDetails: newsList[]=[
+    {
+    newsImg: '/assets/images/news1.jpg',
+    newsTopic: 'You will vainly look for fruit on it in autumn.',
+    newsAuthor: 'Admin',
+    newsDate: '10/02/2022',
+    newsContent: 'Vivamus lacus enim, pulvinar vel nulla sed, scelerisque rhoncus nisi. Praesent vitae mattis nunc, egestas viverra eros.',
+    },
+    {
+      newsImg: '/assets/images/news2.jpg',
+      newsTopic: "A man's worth has its season, like tomato.",
+      newsAuthor: 'Admin',
+      newsDate: '10/02/2022',
+      newsContent: 'Vivamus lacus enim, pulvinar vel nulla sed, scelerisque rhoncus nisi. Praesent vitae mattis nunc, egestas viverra eros.',
+      },
+      {
+        newsImg: '/assets/images/news3.jpg',
+        newsTopic: 'Good thoughts bear good fresh juicy fruit.',
+        newsAuthor: 'Admin',
+        newsDate: '10/02/2022',
+        newsContent: 'Vivamus lacus enim, pulvinar vel nulla sed, scelerisque rhoncus nisi. Praesent vitae mattis nunc, egestas viverra eros.',
+        },
+        {
+          newsImg: '/assets/images/news1.jpg',
+          newsTopic: 'You will vainly look for fruit on it in autumn.',
+          newsAuthor: 'Admin',
+          newsDate: '10/02/2022',
+          newsContent: 'Vivamus lacus enim, pulvinar vel nulla sed, scelerisque rhoncus nisi. Praesent vitae mattis nunc, egestas viverra eros.',
+          },
+          {
+            newsImg: '/assets/images/news2.jpg',
+            newsTopic: "A man's worth has its season, like tomato.",
+            newsAuthor: 'Admin',
+            newsDate: '10/02/2022',
+            newsContent: 'Vivamus lacus enim, pulvinar vel nulla sed, scelerisque rhoncus nisi. Praesent vitae mattis nunc, egestas viverra eros.',
+            },
+            {
+              newsImg: '/assets/images/news3.jpg',
+              newsTopic: 'Good thoughts bear good fresh juicy fruit.',
+              newsAuthor: 'Admin',
+              newsDate: '10/02/2022',
+              newsContent: 'Vivamus lacus enim, pulvinar vel nulla sed, scelerisque rhoncus nisi. Praesent vitae mattis nunc, egestas viverra eros.',
+              }
   ]
 }
